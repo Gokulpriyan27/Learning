420a41bc33d4859e185cd8d6135ad4c2ee757ed8
Gokulpriyan27 <107754768+Gokulpriyan27@users.noreply.github.com>
2022-06-20 21:55:51 +0530

    Add files via upload

---
+++ Resume in JSON Format/script.js
@@ -0,0 +1,31 @@
+var resume={
+  about:{
+    name:"Gokulpriyan N",
+    softskills: "Quick learner, Self-motivated, Initiator, Innovator, Team Builder",
+    contact:{
+      email:"gokulpriyan18@gmail.com",
+      Linkedin:"https://www.linkedin.com/in/gokulpriyan-n-3b8103108",
+      Phone:"73730 82451"},
+    objective: "Energetic MERN stack developer, who is actively looking for an opportunity in web development.",
+    Qualification:"Bachelor's in Mechanical Engineering, Anna University",
+  },
+  Work_Experience:{
+    exp1:{
+      organisation:"HCL Technologies",role:"Lead Engineer in Automation & Design support",Tenure:"Jan 2022 - Present"}
+      ,
+    exp2:{
+      organisation:"Caterpillar India Pvt Ltd",role:"Parts Information Analyst",Tenure:"Nov 2019 - Jan 2022"},
+
+    exp3:{
+      organisation:"CSS Corp",role:"Technical Support Engineer - Trainee",Tenure:"May 2019 - Nov 2019"}
+  },
+  Techanical_skills:{frontend:"HTML5, CSS3, BootStrap, Javascript",backend:"Node.js",database:"MongoDB",Cloud:"AWS, GCP",
+                      Programming:"Python, Visual Basics, basics of C & C++"},
+  certifications:"1.Cambridge Business Preliminary and Business Vantage, 2. IIT Certification in Python, 3.Diploma in C",
+  projects:{College:"Electric Solar Vehicle Championship, prototype on Beam Engine & more..."},
+  Detailed_qualification:{under_graduate:"B.E. Mechanical Engineering from Sri Krishna college of Technology, CGPA --> 8.9/10",
+                          higher_secondary:"Vidyaa vikas boys hr.sec.school, percentage --> 93",
+                          secondary:"Kongu National Matri hr.sec.school, percentage --> 97"},
+  Personal_profile:{Language:"English(Professsional level), Tamil(native)"}
+};
+console.log(resume);
